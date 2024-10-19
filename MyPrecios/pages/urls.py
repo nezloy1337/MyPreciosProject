b6287d6210290df20f898aa4a9646c8defb4529c
Andrian Yukhnevich <andy.yukh@gmail.com>
2024-10-19 11:21:34 +0300

    + main page (html,css) + register page (html,css)

--- MyPrecios/pages/urls.py
+++ MyPrecios/pages/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.home_page, name='home_page'),
+   path('main/', views.main_page, name='main_page'),
+   path('register/', views.register_page, name='register_page'),
 ]
--- MyPrecios/pages/views.py
+++ MyPrecios/pages/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'pages/home_page.html',context={})
+
+def main_page(request):
+    return render(request, 'pages/main_page.html',context={})
+
+def register_page(request):
+    return render(request, 'pages/register_page.html',context={})
