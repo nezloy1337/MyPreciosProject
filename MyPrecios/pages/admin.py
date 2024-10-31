10f05878fdcd23f8d9a7826280345ec1970c1962
Andrian Yukhnevich <andy.yukh@gmail.com>
2024-11-01 01:03:33 +0300

    register mails in admin

--- MyPrecios/pages/admin.py
+++ MyPrecios/pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+# Импорт модели MyModel из текущего каталога (".")
+from .models import Mails
+# Регистрация модели MyModel для административного сайта
+admin.site.register(Mails)
