96d6be5dce51ffdfec3a829c85bdda0b529cd042
Andrian Yukhnevich <andy.yukh@gmail.com>
2024-11-03 20:35:29 +0300

    + restriction to 25 charaters to view

--- MyPrecios/pages/forms.py
+++ MyPrecios/pages/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from pages.models import Mails
 
 
