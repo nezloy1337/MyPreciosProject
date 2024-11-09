3bdd9f500859834f9a5b0c525face1a16dba962e
Andrian Yukhnevich <andy.yukh@gmail.com>
2024-11-09 23:36:18 +0300

    first url test

--- MyPrecios/pages/tests/test_urls.py
+++ MyPrecios/pages/tests/test_urls.py
@@ -1,7 +1,12 @@
+
+
 from django.test import TestCase
+from django.urls import reverse, resolve
+from pages.views import *
 
 
 class TestUrls(TestCase):
 
     def test_main_page(self):
-        assert 1 == 1
+        url = reverse('home')
+        self.assertEqual(resolve(url).func.view_class, MainPage)
